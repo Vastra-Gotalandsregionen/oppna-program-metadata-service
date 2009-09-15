6fa5627f51db19bd78693beeef8340a5fcbb95c1
sture.svensson@findwise.se <sture.svensson@findwise.se@2011de24-316a-11de-9e80-b52b74bdfc52>
2009-09-15 09:43:00 +0000

    La till ResponseObject som är huvudtypen för alla ResponseObject samt NodeListResponseObject som skall returneras av funtioner som även vill returnera en lista av noder

---
+++ trunk/KeywordService/composites/types/src/main/java/se/vgregion/metaservice/keywordservice/domain/ResponseObject.java
@@ -0,0 +1,85 @@
+package se.vgregion.metaservice.keywordservice.domain;
+
+/**
+ * This class should be the basis of all responseObjects, 
+ * functions that wants to add additional return values (such as lists etc)
+ * to the responseobject should extend this class
+ */
+public class ResponseObject {
+
+    /**
+     * This is the set of possible statusmessages in a responseobject
+     */
+    public enum StatusCode {
+
+        ok(200),
+        unsupported_text_format(1001),
+        error_formatting_content(1002),
+        error_processing_content(2001),
+        error_getting_keywords_from_taxonomy(3001),
+        unknown_error(6001);
+        private final int code;
+
+        StatusCode(int code) {
+            this.code = code;
+        }
+
+        /**
+         * Get the status code as an integer
+         * @return the code
+         */
+        public int code() {
+            return code;
+        }
+    }
+    private String errorMessage = null;
+    private StatusCode statusCode = null;
+
+    /**
+     * Empty constructor for ok message
+     */
+    public ResponseObject() {
+        statusCode = StatusCode.ok;
+    }
+
+    /**
+     * Constructor for error-response
+     */
+    public ResponseObject(StatusCode statusCode, String errorMessage) {
+        this.errorMessage = errorMessage;
+        this.statusCode = statusCode;
+    }
+
+    /**
+     * Get the statusCode for this response
+     * @return the statusCode
+     */
+    public StatusCode getStatusCode() {
+        return statusCode;
+    }
+
+    /**
+     * Set the statusCode for this responseObject
+     * @param statusCode the statusCode to set
+     */
+    public void setErrorCode(StatusCode errorCode) {
+        this.statusCode = errorCode;
+    }
+
+    /**
+     * Get the errorMessage for this responseObject
+     * @return the errorMessage if this responseObject has one
+     * null otherwise
+     */
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    /**
+     * Set the errorMessage for this responseObject
+     * @param errorMessage the errorMessage to set
+     */
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
