7784a87e99b6e8c85363f45be614c9ddb5da3d8d
sture.svensson@findwise.se <sture.svensson@findwise.se@2011de24-316a-11de-9e80-b52b74bdfc52>
2009-09-15 16:51:46 +0000

    ändrat getVocabulary så att den kompilerar med den nya api-designen

--- trunk/VocabularyService/modules/intsvc/src/main/java/se/vgregion/metaservice/vocabularyservice/intsvc/VocabularyServiceIntServiceImpl.java
+++ trunk/VocabularyService/modules/intsvc/src/main/java/se/vgregion/metaservice/vocabularyservice/intsvc/VocabularyServiceIntServiceImpl.java
@@ -3,6 +3,7 @@ package se.vgregion.metaservice.vocabularyservice.intsvc;
 import java.util.List;
 
 import se.vgregion.metaservice.keywordservice.domain.MedicalNode;
+import se.vgregion.metaservice.keywordservice.domain.NodeListResponseObject;
 import se.vgregion.metaservice.keywordservice.schema.MedicalNodeSdoHelper;
 import se.vgregion.metaservice.schema.medicalnode.MedicalNodeListType;
 import se.vgregion.metaservice.vocabularyservice.VocabularyService;
@@ -13,10 +14,19 @@ public class VocabularyServiceIntServiceImpl implements se.vgregion.metaservice.
 	private VocabularyService vocabularyService;
 	
 	public MedicalNodeListType getVocabulary(GetVocabularyRequest parameters) {
-		List<MedicalNode> nodes = vocabularyService.getVocabulary(parameters.getPath());
+
+                //TODO: Fix this according to api
+
+		NodeListResponseObject res = vocabularyService.getVocabulary("x",parameters.getPath());
+                List<MedicalNode> nodes = res.getNodeList();
+
 		MedicalNodeListType nodeList = MedicalNodeSdoHelper.toMedicalNodeListType(nodes);
 		return nodeList;
 	}
+
+
+        //TODO: add the rest of the methods according to api
+
 	
 	public void setVocabularyService(VocabularyService vocabularyService) {
 		this.vocabularyService = vocabularyService;
