7dbf75b33b56bd7f3b801434622a99ef5e08ce97
sture.svensson@findwise.se <sture.svensson@findwise.se@2011de24-316a-11de-9e80-b52b74bdfc52>
2009-11-09 14:43:46 +0000

    implemented addVocabularyNode

---
+++ trunk/KeywordService/composites/types/src/main/java/se/vgregion/metaservice/keywordservice/exception/InvalidPropertyTypeException.java
@@ -0,0 +1,9 @@
+package se.vgregion.metaservice.keywordservice.exception;
+
+public class InvalidPropertyTypeException extends Exception{
+
+    public InvalidPropertyTypeException(String message){
+        super(message);
+    }
+
+}
---
+++ trunk/KeywordService/composites/types/src/main/java/se/vgregion/metaservice/keywordservice/exception/NodeAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package se.vgregion.metaservice.keywordservice.exception;
+
+public class NodeAlreadyExistsException extends Exception{
+
+    public NodeAlreadyExistsException(String message){
+        super(message);
+    }
+
+}
