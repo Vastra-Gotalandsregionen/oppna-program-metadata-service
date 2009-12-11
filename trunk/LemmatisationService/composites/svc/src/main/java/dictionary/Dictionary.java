1f997c5e5b1e01ef2bc6c9efc2c30966dc6a07ff
sture.svensson@findwise.se <sture.svensson@findwise.se@2011de24-316a-11de-9e80-b52b74bdfc52>
2009-12-11 09:17:35 +0000

    trie datastructure for lammatisations

---
+++ trunk/LemmatisationService/composites/svc/src/main/java/dictionary/Dictionary.java
@@ -0,0 +1,180 @@
+package dictionary;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+/**
+ *
+ * @author sture.svensson
+ */
+public class Dictionary {
+
+    private Trie root;
+    private Map<String, List<TrieEntry>> map = null;
+    private int lemmas = 0;
+    private int words = 0;
+
+    /**
+     * Create a Dictionary from a file
+     * The file should be newline separated for each word (lemma)
+     * and ","-separated for each variation of that word, the lemma
+     * should come first
+     * @param file the file
+     */
+    public Dictionary(File file) throws IOException {
+        root = new Trie();
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "utf-8"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            line = line.replaceAll("[{}]", "");
+            String[] arr = line.split(",");
+            if (arr[0].equals("")) {
+                continue;
+            }
+            byte[] bArray = line.toLowerCase().getBytes();
+
+            for (int i = 0; i < arr.length; i++) {
+                String norm = arr[i].toLowerCase();
+                TrieEntry te = new TrieEntry();
+                te.setbArray(bArray);
+                root.insert(norm, te);
+                words++;
+            }
+            lemmas++;
+        }
+    }
+
+    /**
+     * Create a dictionary from a List of strings
+     * every string should be a ","-separated list
+     * of all the varaitions of the word, the lemma
+     * should come first
+     * @param wordList the wordlist
+     */
+    public Dictionary(List<String> wordList) {
+        root = new Trie();
+
+        for (String line : wordList) {
+            line = line.replaceAll("[{}]", "");
+            String[] arr = line.split(",");
+            if (arr[0].equals("")) {
+                continue;
+            }
+            byte[] bArray = line.toLowerCase().getBytes();
+            for (int i = 0; i < arr.length; i++) {
+                String norm = arr[i].toLowerCase();
+                TrieEntry te = new TrieEntry();
+                te.setbArray(bArray);
+                root.insert(norm, te);
+                words++;
+            }
+            lemmas++;
+        }
+    }
+
+    /**
+     * Get the words that are a variation of the query, if there are multiple
+     * lemmas with the same word the outer list will have multiple entries.
+     * The lemma is the first entry in every list
+     * @param query the word to look up
+     * @return
+     */
+    public List<List<String>> getWords(String query) {
+        List<List<String>> ret = new LinkedList<List<String>>();
+        for (TrieEntry entry : root.getTrieFor(query)) {
+            List<String> wordList = new LinkedList<String>();
+            String[] arr = new String(entry.getbArray()).split(",");
+            for (int i = 0; i < arr.length; i++) {
+                wordList.add(arr[i]);
+            }
+            ret.add(wordList);
+        }
+        return ret;
+    }
+
+    /**
+     * Get the number of lemmas in the dictionary
+     * @return nr of lemmas
+     */
+    public int getNrOfLemmas() {
+        return lemmas;
+    }
+
+    /**
+     * Get the total number of words in the dictionary
+     * @return total number of words in the dictionary
+     */
+    public int getNrOfWords() {
+        return words;
+    }
+
+    /**
+     * @deprecated
+     * Build a dictionary using a hashmap instead, used for testing/comparison
+     * @param file
+     * @param useHashmap
+     * @throws IOException
+     */
+    public Dictionary(File file, Boolean useHashmap) throws IOException {
+        root = new Trie();
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "utf-8"));
+        String line;
+        map = new HashMap<String, List<TrieEntry>>();
+        while ((line = br.readLine()) != null) {
+            line = line.replaceAll("[{}]", "");
+            String[] arr = line.split(",");
+            if (arr[0].equals("")) {
+                continue;
+            }
+            byte[] bArray = line.toLowerCase().getBytes();
+
+            for (int i = 0; i < arr.length; i++) {
+                String norm = arr[i].toLowerCase();
+                TrieEntry te = new TrieEntry();
+                te.setbArray(bArray);
+                if (map.get(norm) == null) {
+                    List<TrieEntry> list = new Vector<TrieEntry>(1, 1);
+                    list.add(te);
+                    map.put(norm, list);
+                } else {
+                    map.get(norm).add(te);
+                }
+                words++;
+            }
+            lemmas++;
+        }
+    }
+
+    /**
+     * @deprecated
+     * Use this function when the dictionary was built with a hashmap instead of a trie
+     * used for testing/comparison
+     * @param word
+     * @param useHashMap
+     * @return
+     */
+    public List<List<String>> getWords(String query, Boolean useHashMap) {
+        List<List<String>> ret = new LinkedList<List<String>>();
+
+        List<TrieEntry> list = map.get(query);
+        if (list != null) {
+            for (TrieEntry entry : list) {
+                String[] arr = new String(entry.getbArray()).split(",");
+                List<String> wordList = new LinkedList<String>();
+                for (int i = 0; i < arr.length; i++) {
+                    wordList.add(arr[i]);
+                }
+                ret.add(wordList);
+            }
+        }
+        return ret;
+    }
+}
---
+++ trunk/LemmatisationService/composites/svc/src/main/java/dictionary/Main.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dictionary;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author sture.svensson
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        File file = new File("saldo_1.0.txt");
+        Dictionary dict = null;
+        long start = new Date().getTime();
+        try {
+            dict = new Dictionary(file);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+        long time = new Date().getTime() - start;
+
+        /*
+        List<String> list = new ArrayList<String>();
+        list.add("apa,apan");
+        list.add("gris,grisen");
+        list.add("g-ris,grisen");
+        Dictionary dict = new Dictionary(list);
+         */
+
+        System.out.println("Initialized a dictionary with " + dict.getNrOfLemmas() + " lemmas and " + dict.getNrOfWords() + " words in total" +
+                ", building took " + time +" milliseconds");
+
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            System.out.print("\n>");
+            String input = in.nextLine();
+
+            start = new Date().getTime();
+            List<List<String>> entryList = dict.getWords(input);
+            time = new Date().getTime() - start;
+            System.out.println("Results (query took "+time+" milliseconds):");
+            for (List<String> e : entryList) {
+                System.out.println("---------");
+                for (String s : e) {
+                    System.out.println(s);
+                }
+            }
+        }
+    }
+}
