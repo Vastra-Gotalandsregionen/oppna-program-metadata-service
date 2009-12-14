53019453daa406f2d21a1c98fa6e0dff29ba0419
johan.sjobergj@gmail.com <johan.sjobergj@gmail.com@2011de24-316a-11de-9e80-b52b74bdfc52>
2009-12-14 11:15:40 +0000

    Fixed the last missing pieces of the lemmatisation service

--- trunk/LemmatisationService/composites/svc/src/main/java/se/vgregion/metaservice/LemmatisationService/LemmatisationSvc.java
+++ trunk/LemmatisationService/composites/svc/src/main/java/se/vgregion/metaservice/LemmatisationService/LemmatisationSvc.java
@@ -20,10 +20,19 @@ public interface LemmatisationSvc {
     public LemmatisedResponse getParadigmsObject(String word);
 
     /**
-     * Initialize the lemmatisation service
+     * Initialize the lemmatisation service with the default
+     * wordlist (saldo.txt).
      *
      * @throws InitializationException
      */
     public void init() throws InitializationException;
-    
+
+    /**
+     * Initialize the lemmatisation service with a custom
+     * location for the wordlist.
+     *
+     * @param wordlistLocation Location of the wordlist
+     */
+    public void init(String wordlistLocation) throws InitializationException;
+
 }
