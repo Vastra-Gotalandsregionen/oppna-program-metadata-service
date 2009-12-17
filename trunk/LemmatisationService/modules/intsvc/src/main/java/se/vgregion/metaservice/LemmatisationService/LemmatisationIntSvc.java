a361e283a8786b08387c18ab188a9ca572ed3e10
johan.sjobergj@gmail.com <johan.sjobergj@gmail.com@2011de24-316a-11de-9e80-b52b74bdfc52>
2009-12-17 18:51:42 +0000

    Exposed XSD schema to the HTTP interface

--- trunk/LemmatisationService/modules/intsvc/src/main/java/se/vgregion/metaservice/LemmatisationService/LemmatisationIntSvc.java
+++ trunk/LemmatisationService/modules/intsvc/src/main/java/se/vgregion/metaservice/LemmatisationService/LemmatisationIntSvc.java
@@ -1,6 +1,7 @@
 
 package se.vgregion.metaservice.LemmatisationService;
 
+import java.io.Writer;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,5 +9,18 @@ import org.springframework.web.servlet.ModelAndView;
  * The Lemmatisation service external interface
  */
 public interface LemmatisationIntSvc {
+    /**
+     * Retrieve the lemmatisation and paradigms of the input word
+     *
+     * @param word Word to lemmatise and get paradigms for
+     * @return
+     */
     public ModelAndView getParadigms(@PathVariable String word);
+
+    /**
+     * Retrieve the XSD schema for the XML returned from getParadigms
+     *
+     * @return
+     */
+    public void getXMLSchema(Writer writer);
 }
--- trunk/LemmatisationService/modules/intsvc/src/main/java/se/vgregion/metaservice/LemmatisationService/intsvc/LemmatisationIntSvcImpl.java
+++ trunk/LemmatisationService/modules/intsvc/src/main/java/se/vgregion/metaservice/LemmatisationService/intsvc/LemmatisationIntSvcImpl.java
@@ -1,6 +1,18 @@
 
 package se.vgregion.metaservice.LemmatisationService.intsvc;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Writer;
+import java.util.logging.Level;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -29,12 +41,35 @@ public class LemmatisationIntSvcImpl implements LemmatisationIntSvc {
      * @param word The word to lemmatise
      * @return
      */
-    @RequestMapping(value="/{word}", method=RequestMethod.GET)
+    @RequestMapping(value="/lemmatisation/{word}", method=RequestMethod.GET)
     public ModelAndView getParadigms(@PathVariable String word) {
         LemmatisedResponse response = lemmationsationSvc.getParadigmsObject(word);
 
         // Include BindingResult.MODEL_KEY_PREFIX due to a bug in MarshallingView.java ?
         return new ModelAndView("jaxbView", BindingResult.MODEL_KEY_PREFIX + "paradigmsresponse", response);
+    }
+
+    @RequestMapping(value="/schema.xsd", method=RequestMethod.GET)
+    public void getXMLSchema(Writer writer) {
+        File xsdFile = new File("E:\\projects\\oppna-program-metadata-service\\LemmatisationService\\modules\\intsvc\\target\\MetaService-LemmatisationService-module-intsvc\\WEB-INF\\classes\\schema1.xsd");
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+
+        try {
+            fis = new FileInputStream(xsdFile);
+            bis = new BufferedInputStream(fis, 4096);
+            int theChar;
+            while ((theChar = bis.read()) != -1) {
+                writer.write(theChar);
+            }
+            bis.close();
+            fis.close();  
+        } catch (IOException ex) {
+            try {
+                if (fis != null) bis.close();
+                if (bis != null) fis.close();
+            } catch (IOException e) { }
+        }
     }
 
     @Autowired(required=true)
@@ -46,5 +81,7 @@ public class LemmatisationIntSvcImpl implements LemmatisationIntSvc {
             log.error("Error initializing lemmatisation service", ex);
         }
     }
+
+    
  
 }
